9f2222bad32c572edcf18dcb80aacfd7ddff3e8f
Joseph Kogan <93930094+JKogan2025@users.noreply.github.com>
2022-01-14 22:30:17 +0000

    Continued work on all functions, which should be working except for correct, where there is a list assignment issue to be fixed.

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ Name of Project: Python 1 (Hangman)
 #Write the main part of your program here. Use of the other pages is optional.
 
 from time import sleep
+from page1 import mistakesleft
 import os
 import sys
 
@@ -21,56 +22,61 @@ sleep(2.5)
 x = int(input("Choose an integer from 1-12 to generate a random word for hangman: "))
 x -= 1
 spaces_list = list(list_a[x])
-mistakesleft = 6
+joinedspaces_list = ''.join(spaces_list)
+incorrect_list = []
 
 print("Spaces left in word:")
 originaldashes_list = ["_ " * len(spaces_list)]
 print(*originaldashes_list)
 
 def incorrect(letter):
-  incorrect_list = []
-  incorrect_list.append(letter)
-  mistakesleft = 6
-  mistakesleft -= 1
   print("Sorry, that letter isn't in the word. You have", mistakesleft, "mistakes remaining. The letters you've guessed incorrectly are:", *incorrect_list)
   print("Spaces left in word:")
-  for i in range(1, 2):
-    dog = "_ " * len(spaces_list)
-    print(dog)
+  print(*originaldashes_list)
   if mistakesleft == 0:
     print("You've reached the maximum number of tolerable mistakes. Every normal person now despises you. Goodbye!")
     os.system("clear")
     sys.exit()
+  letter = None
 
 def correct(letter):
-  dash_list = ["_ " * len(spaces_list)]
   print("That's correct!")
   print("Spaces left in word:")
-  dash_list.pop(spaces_list.index(letter))
-  dash_list.insert(spaces_list.index(letter), letter)
-  print(*dash_list)
+  originaldashes_list[spaces_list.index(letter)] = letter
+  print(*originaldashes_list)
+  letter = None
 
-def letterinput(letter):
-  joinedspaces_list = ''.join(spaces_list)
+while mistakesleft != 0 and ''.join(originaldashes_list) != joinedspaces_list:
+  letter = input("Type your letter guess here: ")
   if letter in spaces_list:
     correct(letter)
-  elif len(letter) > 1 and letter == joinedspaces_list:
+  elif letter not in spaces_list:
+    incorrect_list.append(letter)
+    mistakesleft -= 1
+    incorrect(letter)
+  elif len(letter) > 1 and letter != joinedspaces_list:
     print("That guess is incorrect, and the hangman program shall now be terminated.")
+    sleep(2)
     sys.exit()
   elif letter == joinedspaces_list:
     print("Woah, you guessed the full word, and without guessing each letter! The Force is strong with this one. I must tell my master...")
+    sleep(4)
     os.system("clear")
     print("Hangman program has been terminated. Instructions to follow.")
     sys.exit()
+
+if ''.join(originaldashes_list) == joinedspaces_list:
+  print("Great job! You guessed the word! If you would like to play again, simply re-run the program.")
+
+''''
+joinedoriginaldashes_list = ''.join(originaldashes_list)
+while mistakesleft != 0 or joinedspaces_list != joinedoriginaldashes_list:
+  '''
 
 ''''
 from correct import dash_list
 from incorrect import mistakesleft
 '''
-while mistakesleft != 0 or "_ " in dash_list ==  True:
-  l1 = input("Type your letter guess here: ")
-  letterinput(l1)
-  l1 = None
 
 
 #import page2  # uncomment if you're using page2
--- page1.py
+++ page1.py
@@ -1,6 +1,2 @@
 #Use of this page is optional. If you use code here, make sure either import page1 or from page1 import * appear on your main.py page.
-
-import main
-import page1
-import os
-import sys
+mistakesleft = 6
