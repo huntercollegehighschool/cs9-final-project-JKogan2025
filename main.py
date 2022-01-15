b1585716d0238439280e61079fb2e80017411e3a
Joseph Kogan <93930094+JKogan2025@users.noreply.github.com>
2022-01-15 19:03:43 +0000

    Everything except the case where a letter appears multiple times in a word is accounted for. See comments on the correct function for how to fix this issue.

--- main.py
+++ main.py
@@ -12,21 +12,23 @@ import sys
 
 print("Welcome to the best hangman game in the world!")
 sleep(1.5)
-print("The rules are simple: the number of letters in the generated word will be represented on the screen as dashes, and you will have to guess each letter to win! If at any time you want to guess the word in it's entirety you may do so, but you may only have one guess. If that guess is wrong, you lose. Beware, if you make six letter mistakes, you lose.")
+print("The rules are simple: the number of letters in the generated word will be represented on the screen as dashes, and you will have to guess each letter to win! If at any time you want to guess the word in its entirety you may do so, but you may only have one guess. If that guess is wrong, you lose. Beware, if you make six letter mistakes, you lose.")
 sleep(1.5)
 print("Without further ado, good luck!")
 
 list_a = ["conspicuous", "alligator", "connoisseur", "detrimental", "vivacious", "presidential", "collection", "lackadaisical", "fuchsia", "thumbscrew", "lymph", "marquis"]
 
 sleep(2.5)
-x = int(input("Choose an integer from 1-12 to generate a random word for hangman: "))
+x = int(input("Choose an integer from 1-12 to generate a word for hangman: "))
 x -= 1
 spaces_list = list(list_a[x])
 joinedspaces_list = ''.join(spaces_list)
 incorrect_list = []
 
 print("Spaces left in word:")
-originaldashes_list = ["_ " * len(spaces_list)]
+originaldashes_list = ["_ "]
+for i in range(1, len(spaces_list)):
+  originaldashes_list.append("_ ")
 print(*originaldashes_list)
 
 def incorrect(letter):
@@ -40,9 +42,15 @@ def incorrect(letter):
   letter = None
 
 def correct(letter):
+  while letter in spaces_list:
+    indicestoreplace = []
+    index = spaces_list.index(letter)
+    indicestoreplace.append(index)
+    spaces_list.pop(index)
+  #letter_index = [z for z, x in enumerate(spaces_list) if x == letter]
   print("That's correct!")
   print("Spaces left in word:")
-  originaldashes_list[spaces_list.index(letter)] = letter
+  #originaldashes_list = [letter if letter == letter_index else letter for letter in originaldashes_list]
   print(*originaldashes_list)
   letter = None
 
@@ -50,8 +58,14 @@ while mistakesleft != 0 and ''.join(originaldashes_list) != joinedspaces_list:
   letter = input("Type your letter guess here: ")
   if letter in spaces_list:
     correct(letter)
-  elif letter not in spaces_list:
+  elif len(letter) == 1 and letter not in spaces_list and len(incorrect_list) == 0:
     incorrect_list.append(letter)
+    mistakesleft -= 1
+    incorrect(letter)
+  elif len(letter) == 1 and letter not in spaces_list and len(incorrect_list) > 0:
+    incorrect_listletters = [",", letter]
+    incorrect_list.extend(incorrect_listletters)
+    incorrect_listletters = None
     mistakesleft -= 1
     incorrect(letter)
   elif len(letter) > 1 and letter != joinedspaces_list:
